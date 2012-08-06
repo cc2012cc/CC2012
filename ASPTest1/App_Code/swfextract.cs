e890f9aad5927646cdbc889994191b4ad8a0f1cb
Andong.Li <43534545@qq.com>
2012-08-06 05:34:01 -0400

    first commit

---
+++ ASPTest1/App_Code/swfextract.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lib
+{
+    public class swfextract
+    {
+        protected lib.Config configManager;
+
+        public swfextract(String mapPath)
+        {
+            configManager = new Config(mapPath);
+        }
+
+        public String extractText(String doc, String page)
+        {
+            try
+            {
+                String output = "";
+                String swfFilePath = configManager.getConfig("path.swf") + doc + page + ".swf";
+                String command = configManager.getConfig("cmd.searching.extracttext"); ;
+            
+                if(!Common.validSwfParams(swfFilePath,doc,page))
+                    return "[Invalid Parameters]";
+
+                command = command.Replace("{path.swf}",configManager.getConfig("path.swf"));
+                command = command.Replace("{swffile}", doc + page + ".swf");
+
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo.FileName = command.Substring(0, command.IndexOf(".exe") + 5);
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.Arguments = command.Substring(command.IndexOf(".exe") + 5);
+
+                if (proc.Start())
+                {
+                    output = proc.StandardOutput.ReadToEnd();
+                    return output;
+                }
+                else
+                    return "[Error Extracting]";
+            }
+            catch (Exception ex)
+            {
+                return "[Error Extracting]";
+            }
+        }
+    }
+}
---
+++ ASPTest1/change_config.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using lib;
+
+public partial class change_config : System.Web.UI.Page
+{
+    protected Config configManager;
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        configManager = new Config(Server.MapPath(VirtualPathUtility.GetDirectory(Request.Path)));
+
+        if (configManager.getConfig("admin.password") == null)
+        {
+            Response.Redirect("setup.aspx");
+            Response.End();
+        }
+
+        if (Request["SAVE_CONFIG"] != null)
+        {
+            configManager.setConfig("path.pdf", Request["PDF_Directory"]);
+            configManager.setConfig("path.swf", Request["SWF_Directory"]);
+            configManager.setConfig("licensekey", Request["LICENSEKEY"]);
+            configManager.setConfig("splitmode", Request["SPLITMODE"]);
+            configManager.setConfig("renderingorder.primary", Request["RenderingOrder_PRIM"]);
+            configManager.setConfig("renderingorder.secondary", Request["RenderingOrder_SEC"]);
+
+            configManager.SaveConfig(Server.MapPath(VirtualPathUtility.GetDirectory(Request.Path)));
+
+            // wipe old converted files
+            if (System.IO.Directory.Exists(configManager.getConfig("path.swf")))
+            {
+                foreach (string filename in System.IO.Directory.GetFiles(configManager.getConfig("path.swf"))) 
+                {
+                    System.IO.File.Delete(filename);
+                }
+            }
+
+            Response.Redirect("Default.aspx?msg=Configuration%20saved!");
+        }
+
+        if (Session["FLEXPAPER_AUTH"] == null)
+        {
+            Response.Redirect("Default.aspx");
+            Response.End();
+        }
+    }
+}
